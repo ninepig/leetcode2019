14522004ae04a1bad987d99a70734ce7bed66a27
ninepig <ywj1988>
2019-05-07 23:03:54 -0400

    Update distanceOfTwoPoint.java

--- src/amazon/MianJing/distanceOfTwoPoint.java
+++ src/amazon/MianJing/distanceOfTwoPoint.java
@@ -8,33 +8,33 @@ import java.util.Queue;
  */
 public class distanceOfTwoPoint {
     // without weight , we use bfs
-    public int distanceBetweenTwoCitiesWithOutWeight(int[][] cityGraph,int start , int end){
-        if(cityGraph == null || cityGraph.length == 0){
+    public int distanceBetweenTwoCitiesWithOutWeight(int[][] cityGraph, int start, int end) {
+        if (cityGraph == null || cityGraph.length == 0) {
             return 0;
         }
         int[] visited = new int[cityGraph.length];
         int sum = 0;
         LinkedList<city> queue = new LinkedList<>();
-        queue.offer(new city(start,0));
+        queue.offer(new city(start, 0));
         visited[start] = 1;
         boolean flag = false;
         int mark = 0;
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             city curCity = queue.poll();
-            for (int i = 0 ; i < cityGraph[curCity.x].length ; i++){
-                if(cityGraph[curCity.x][i] == 1 && visited[i] == 0){
-                    mark =  i;
+            for (int i = 0; i < cityGraph[curCity.x].length; i++) {
+                if (cityGraph[curCity.x][i] == 1 && visited[i] == 0) {
+                    mark = i;
                     sum = curCity.s + 1;
-                    queue.offer(new city(i,sum));
+                    queue.offer(new city(i, sum));
                     visited[i] = 1;
                 }
-                if(mark == end){
+                if (mark == end) {
                     flag = true;
                     break;
                 }
             }
-            if(flag){
+            if (flag) {
                 break;
             }
         }
@@ -44,9 +44,46 @@ public class distanceOfTwoPoint {
     class city {
         int x;
         int s;
+
         city(int x, int s) {
             this.x = x;
             this.s = s;
         }
+    }
+
+    int min = 0;
+
+    public int distanceBetweenTwoCitiesWithWeight(int[][] cityGraph, int start, int end) {
+        if (cityGraph == null || cityGraph.length == 0) return 0;
+        int[] visisted = new int[cityGraph.length];
+        dfs(cityGraph, start, end, visisted, 0);
+
+        return min;
+    }
+
+    private void dfs(int[][] cityGraph, int cur, int end, int[] visisted, int dis) {
+        // if current distance is bigger than the current minimum,  return
+        if (dis > min) {
+            return;
+        }
+        // if it is the target city, and update the minvalue
+        if(cur == end){
+            if(dis < min){
+                min  = dis;
+                return ;
+            }
+        }
+        /**
+         * 当前点到其他各点之间可连通但是还未添加进来时，遍历执行
+         * */
+        for (int i = 1; i <= cityGraph.length; i++) {
+            if (cityGraph[cur][i] != 99999999 && visisted[i] == 0) {
+                visisted[i] = 1;
+                dfs(cityGraph,i,end,visisted,dis+cityGraph[cur][i]);
+                visisted[i] = 0;
+            }
+        }
+        return ;
+
     }
 }
